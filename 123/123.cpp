34d3758e6601acff91693c575c914b4195839002
AlyonaShikaeva <71411628+AlyonaShikaeva@users.noreply.github.com>
2020-09-25 17:17:41 +0300

    first commit

---
+++ 123/123.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int a, b, S, P;
+	printf("Введите стороны прямоугольника\n");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	S = a * b;
+	printf("Площадь прямоугольника=%d\n", S);
+	P = 2 * (a + b);
+	printf("Периметр прямоугольника=%d\n", P);
+	return 0;
+}
